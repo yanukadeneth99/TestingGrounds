eaaa0441908d70ba25adf702a8921ad151c11433
yanukadeneth99 <yanukadeneth99@gmail.com>
2020-04-29 16:00:00 +0530

    Worked on the BP to ChooseNextWayPoint and made the C++ file for it.

---
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+//class UBTService;
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTNode
+{
+	GENERATED_BODY()
+	
+};
