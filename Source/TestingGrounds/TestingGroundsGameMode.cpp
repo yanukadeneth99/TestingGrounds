50b817c42a883e015c29803524339928d3839fa5
yanukadeneth99 <yanukadeneth99@gmail.com>
2020-05-13 12:22:45 +0530

    Removed the Trash Folder

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -9,7 +9,8 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Blueprints/Character_BP"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
