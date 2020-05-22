089ca04fd4bd37dd60073f08d3da1fc3e9af7d4a
yanukadeneth99 <yanukadeneth99@gmail.com>
2020-05-22 22:00:28 +0530

    Worked on a place actor function which spawns an actor multiple specified time per tile.

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -2,26 +2,56 @@
 
 
 #include "Tile.h"
+#include "Engine/World.h"
+
 
 // Sets default values
-ATile::ATile()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ATile::ATile() {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
 // Called when the game starts or when spawned
-void ATile::BeginPlay()
-{
+void ATile::BeginPlay() {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
-void ATile::Tick(float DeltaTime)
-{
+void ATile::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 }
 
+/* Place an Actor in a Randomly generated location within the boundaries of the tile */
+void ATile::PlaceActor(TSubclassOf<AActor> ObjectToSpawn, int MinSpawn, int MaxSpawn) {
+
+	// Write the Ranges of the Box (Static because you don't need to change the ranges)
+	FVector MinBounds(-2000.0f, 2000.0f, 100.f);
+	FVector MaxBounds(2000.0f, -2000.0f, 100.f);
+
+	// Make a Box based on the Min and Max
+	FBox Bounds(MinBounds, MaxBounds);
+
+	// Loop to make the object produce multiple times
+	int LoopRange = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (int i = 0; i < LoopRange; i++) {
+
+		// Calculate the Random FVector point in between the given ranges
+		FVector CalculatedBox(FMath::RandPointInBox(Bounds));
+
+		// Spawn the Actor
+		AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ObjectToSpawn);
+
+		// Specify the objects Transform
+		SpawnedActor->SetActorRelativeLocation(CalculatedBox);
+
+		// Attach the Actor into the Tile Blueprint while keeping its relative location and physics properties
+		SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+
+		// Add the object into the array
+		SpawnedObjects.Add(SpawnedActor);
+
+	}
+}
--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -7,11 +7,12 @@
 #include "Tile.generated.h"
 
 UCLASS()
-class TESTINGGROUNDS_API ATile : public AActor
-{
+class TESTINGGROUNDS_API ATile : public AActor {
+
 	GENERATED_BODY()
-	
-public:	
+
+
+public:
 	// Sets default values for this actor's properties
 	ATile();
 
@@ -19,7 +20,15 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	// Place an Actor in a Randomly generated location within the boundaries of the tile
+	UFUNCTION(BlueprintCallable, Category = Props)
+	void PlaceActor(TSubclassOf<AActor> ObjectToSpawn, int MinSpawn, int MaxSpawn);
+
+	/* Contains the number of AActor Objects spawned via the PlaceActor funtion */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Props)
+	TArray<AActor*> SpawnedObjects; // Does this need to be accessed from public (Make a getter if so)
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
